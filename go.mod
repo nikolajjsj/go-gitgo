14b825bf87287091ec1a8bf6b037b4b21bd1d527
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2021-10-19 15:39:00 +0200

    init go module and implement flag functionality

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/go-gitgo
+
+go 1.17
+
+require github.com/ogier/pflag v0.0.1 // indirect
--- main.go
+++ main.go
@@ -2,8 +2,31 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
+
+	flag "github.com/ogier/pflag"
+)
+
+// flags
+var (
+	user string
 )
 
 func main() {
-	fmt.Println("Hello, world!")
+	flag.Parse()
+
+	if flag.NFlag() == 0 {
+		fmt.Printf("Usage: %s [options]\n", os.Args[0])
+		fmt.Println("Options:")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	var users = strings.Split(user, ",")
+	fmt.Printf("Searching user(s): %s\n", users)
+}
+
+func init() {
+	flag.StringVarP(&user, "user", "u", "", "Search Users")
 }
