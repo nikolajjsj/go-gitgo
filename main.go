7a29aa50b334e138cef93571335a63b5d452c650
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2021-10-19 16:04:49 +0200

    Implement user search functionality

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"strings"
 
+	colour "github.com/fatih/color"
 	flag "github.com/ogier/pflag"
 )
 
@@ -17,10 +18,20 @@ var (
 func main() {
 	flag.Parse()
 
-	hasFlags()
+	if flag.NFlag() == 0 {
+		noFlags()
+	}
 
 	var users = strings.Split(user, ",")
 	fmt.Printf("Searching user(s): %s\n", users)
+	for _, u := range users {
+		result := GetUsers(u)
+		colour.Cyan(`Username:	%s`, result.Login)
+		colour.Blue(`Name:		%s`, result.Name)
+		colour.Green(`Email:		%s`, result.Email)
+		colour.HiMagenta(`Bio:		%s`, result.Bio)
+		fmt.Println("")
+	}
 }
 
 func init() {
@@ -28,11 +39,9 @@ func init() {
 	flag.StringVarP(&repository, "repository", "r", "", "GitHub repository")
 }
 
-func hasFlags() {
-	if flag.NFlag() == 0 {
-		fmt.Printf("Usage: %s [options]\n\n", os.Args[0])
-		fmt.Println("Options:")
-		flag.PrintDefaults()
-		os.Exit(1)
-	}
+func noFlags() {
+	fmt.Printf("Usage: %s [options]\n\n", os.Args[0])
+	fmt.Println("Options:")
+	flag.PrintDefaults()
+	os.Exit(1)
 }
